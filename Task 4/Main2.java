f8847d2098456a2810917245c01356f2a3b85493
Wasima <wasimasultana230@gmail.com>
2026-06-23 10:39:35 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,44 @@
 
+class TeamMember {
+    protected String name;
+
+    public TeamMember(String name) {
+        this.name = name;
+    }
+}
+
+interface BounsEligible {
+    void calculateBouns();
+}
+
+class Employee extends TeamMember implements BounsEligible {
+    public Employee(String name) {
+        super(name);
+    }
+
+    public void calculateBouns() {
+        System.out.println("Calculating standard employee bouns...");
+    }
+}
+
+class Contractor extends TeamMember {
+    public Contractor(String name) {
+        super(name);
+    }
+
+    public void submitInvoice() {
+        System.out.println("Submitting contractor invoice..");
+    }
+}
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        Employee emp = new Employee("Alice");
+        emp.calculateBouns();
+
+        Contractor con = new Contractor("Bob");
+        con.submitInvoice();
+    }
+}
+
