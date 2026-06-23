3d0c7d110f48d17682b6a72591c568e4e6b57010
Wasima <wasimasultana230@gmail.com>
2026-06-23 11:36:27 +0600

    Update Main4.java

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,36 @@
 
+public class Main4 {
+    public static void main(String[] args) {
+        MessageSender email = new EmailSender();
+        NotificationService service1 = new NotificationService(email);
+        service1.alterUser("Hello via Email");
+
+        MessageSender sms = new SMSSender();
+        NotificationService service2 = new NotificationService(sms);
+        service2.alterUser("Hello via SMS");
+
+    }
+}
+ interface MessageSender{
+    void sendMessage(String message);
+} 
+
+class EmailSender implements MessageSender{
+    public void sendMessage(String message){
+        System.out.println("Sending email: "+message);
+    }
+}
+class SMSSender implements MessageSender{
+    public void sendMessage(String message){
+        System.out.println("Sending SMS: "+ message);
+    }
+}
+class NotificationService {
+    private MessageSender sender;
+    public NotificationService(MessageSender sender){
+        this.sender = sender;
+    }
+    public void alterUser(String msg){
+        sender.sendMessage(msg);
+    }
+}
