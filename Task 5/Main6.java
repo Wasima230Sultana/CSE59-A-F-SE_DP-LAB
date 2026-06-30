a5b007ff7a02725705d00f1bd6a7bc431ad3213b
Wasima <wasimasultana230@gmail.com>
2026-06-30 11:26:19 +0600

    Update Main6.java

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,49 @@
 
+
+interface HttpService {
+
+    void handleHttpRequest();
+}
+
+interface DatabaseManager {
+
+    void executeSqlStatement();
+}
+
+interface ContainerOps {
+
+    void restartDockerContainer();
+}
+
+class WebController implements HttpService {
+
+    public void handleHttpRequest() {
+        System.out.println("Routing traffic to endpoint.");
+    }
+}
+
+class SqlDatabase implements DatabaseManager {
+
+    public void executeSqlStatement() {
+        System.out.println("Executing SQL statement");
+    }
+}
+
+class DockerManager implements ContainerOps {
+
+    public void restartDockerContainer() {
+        System.out.println("Restarting Docker container.");
+    }
+}
+
+public class Main6 {
+
+    public static void main(String[] args) {
+        HttpService controller = new WebController();
+        DatabaseManager database = new SqlDatabase();
+        ContainerOps docker = new DockerManager();
+        controller.handleHttpRequest();
+        database.executeSqlStatement();
+        docker.restartDockerContainer();
+    }
+}
