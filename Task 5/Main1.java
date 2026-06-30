539923134d6e36914bc9a405e46d37da572fc830
Wasima <wasimasultana230@gmail.com>
2026-06-30 11:23:28 +0600

    Update Main1.java

--- Task 5/Main1.java
+++ Task 5/Main1.java
@@ -1,1 +1,43 @@
+interface Printer {
+    void printDocument();
+}
+interface Scanner{
+    void scanDocument();
+}
+interface FaxMachine{
+ void faxDocument();
+}
 
+class MultiFunctionCopier implements Printer, Scanner, FaxMachine{
+
+    public void printDocument(){
+        System.out.println("Printing ...");
+    }
+    public void scanDocument(){
+        System.out.println("Scanning ...");
+    }
+    public void faxDocument(){
+        System.out.println("Faxing ...");
+    }
+} 
+
+class BasicPrinter implements Printer{
+    public void printDocument(){
+        System.out.println("Printing...");
+    }
+}
+
+public class Main1{
+    public static void main(String[] args) {
+        System.out.println("Basic Printer");
+        Printer basicPrinter = new BasicPrinter(); 
+        basicPrinter.printDocument();
+        System.out.println();
+
+        System.out.println("MultiFunction Copier");
+        MultiFunctionCopier copier = new MultiFunctionCopier();
+        copier.printDocument();
+        copier.scanDocument();
+        copier.faxDocument();
+          }
+}
